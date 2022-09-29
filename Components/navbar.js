f6509eece710b424be24622bf990695a6c19d8b6
Vivek Patel <vp133202@gmail.com>
2022-09-29 11:46:54 +0530

    my day-2 work

---
+++ Components/navbar.js
@@ -0,0 +1,60 @@
+function navbar(){
+    return `<div id="vnavbar">
+
+
+    <div id="vtop">
+        <div>
+            <img src="https://cdn-icons-png.flaticon.com/512/2420/2420893.png" alt="">
+            <p>Download App</p>
+        </div>
+        <div>
+            <img src="https://cdn-icons-png.flaticon.com/512/747/747376.png" alt="">
+            <p>For Employers</p>
+        </div>
+        <div>
+            <img src="https://cdn-icons-png.flaticon.com/512/3410/3410021.png" alt="">
+            <p>Help</p>
+        </div>
+    </div>
+
+
+    <div id="vmain">
+        <div id="vmain-1">
+            <div>
+                <img src="https://www.shine.com/next/static/images/shine-logo.png" alt="">
+            </div>
+            <div>
+                <div id="vsearch">
+                    <input type="text" placeholder="Job title,skills">
+                    <button>
+                        <img src="https://www.seekpng.com/png/full/920-9209972_magnifying-glass-png-white-search-icon-white-png.png" alt="">
+                    </button>
+                </div>
+                <div>
+                    Trending: | <a href="">React Js</a>  | <a href="">Full-stack Developer</a>  | <a href="">Business Development</a>  | <a href="">Marketing</a>  | <a href="">Finance</a> 
+                </div>
+            </div>
+        </div>
+        <div id="vmain-2">
+            <div>
+                <button>Login</button>
+                <button>Register</button>
+            </div>
+            <div>
+               <img src="https://cdn-icons-png.flaticon.com/128/2529/2529521.png" alt="">
+               <img src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div id="vbot">
+        <a href="">HOME</a>
+        <a href="">JOBS</a>
+        <a href="">JOBS SEEKING ASSISTANCE</a>
+        <a href="">COURSES</a>
+        <a href="">CAREER GUIDANCE</a>
+        <a href="">BLOG</a>
+    </div>
+</div>`
+}
+
+export default navbar;
